6ffb8a967d9e7ad9d322d9463fe79a171f219ead
Jotazin22l <jv.joaovitor4333@gmail.com>
2025-04-26 11:25:46 -0300

    Update Main.java

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,44 @@
+import java.util.Scanner;
 public class Main
 {
+
+
+// exercicio 1
+
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+    char primeiroCharacter= 'J';
+	int idade= 15;
+	boolean tomeiCafe= true;
+	
+	
+//exercicio 2
+
+	//int quantLaranja= 12;
+	//int valorUva= 11;
+	//int somaAmbas=quantLaranja+valorUva;
+	
+	
+//exercicio 3	
+
+//System.out.println(primeiroCharacter);
+//System.out.println(idade);
+//System.out.println(tomeiCafe);
+//System.out.println(somaAmbas);
+
+//exercicio 4
+
+
+Scanner entrada = new Scanner(System.in);
+
+System.out.print("Digite o quantLaranja:" );
+int quantLaranja = entrada.nextInt();
+
+System.out.print("Digite o valorUva:" );
+int valorUva = entrada.nextInt();
+ int soma = quantLaranja + valorUva;
+ System.out.println(soma);
+
+	    
 	}
+	
 }
