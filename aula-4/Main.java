98934f9b3a5813608ad64fafe8d958b810106b33
Jotazin22l <jv.joaovitor4333@gmail.com>
2025-05-24 11:33:30 -0300

    Update Main.java

--- aula-4/Main.java
+++ aula-4/Main.java
@@ -1,7 +1,125 @@
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+// Questao 1. Enquanto
+
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	
+		int x= 0;
+		System.out.println("Abla a apalvra");
+		String letra = input.nextLine();
+		
+		while (x < 5) {
+		    
+		 System.out.println(letra);
+		 x++;
+		}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    
+	    System.out.println("Abla garotinho");
+	    int nume1 = input.nextInt();
+	    
+	    for (int nume2 = 1; nume2 <= 10; nume2++){
+	       System.out.println(nume2*nume1 + " "); 
+	    }
+	
+		
+		
+	}
+}/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	 Scanner input = new Scanner(System.in);
+	System.out.print("Abla garota: ");
+	
+	
+	
+	for(int i = 10; i <= 9; i = i+2) {
+	       System.out.println(i + " "); 
+	}
+	
+	
+	
+	
+	
+	}
+}/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+// Questao 2. 1 a 100 
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	 
+
+	 int y = 1;
+	 int somaTotal = 0;
+	 
+	 while(y <= 100) {
+	 somaTotal += y;
+	 y++;
+	     
+	 	System.out.println(somaTotal);
+	 }
+	}
+}
+
+
+
+
+		
 	}
 }
